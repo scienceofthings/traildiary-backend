a4245a6bfac38e4f64ef96112cc7d2c183c2f2d8
Gregor Hyneck <supergreg@gmx.de>
2021-03-27 18:42:48 +0100

    Add path to files

--- mysite/traildiary/services.py
+++ mysite/traildiary/services.py
@@ -1,10 +1,12 @@
 from django.core.files.storage import FileSystemStorage
 import gpxpy
 import gpxpy.gpx
+from os import path
 
 
 class TrailDirectoryService:
     filesystem = None
+    gpx_file_name_without_path = ''
 
     def __init__(self, directory):
         self.filesystem = FileSystemStorage('uploads/' + directory)
@@ -13,16 +15,23 @@ class TrailDirectoryService:
         return zip(*[iter(lst)]*n)
 
     def get_gpx_file_name(self):
+        filename = self._get_gpx_file_name_without_path()
+        return path.join(path.basename(self.filesystem.base_location), filename)
+
+    def _get_gpx_file_name_without_path(self):
+        if self.gpx_file_name_without_path != '':
+            return self.gpx_file_name_without_path
         for file in self.filesystem.listdir('.')[1]:
             if file.endswith(".gpx"):
-                return file
+                self.gpx_file_name_without_path = file
+                return self.gpx_file_name_without_path
 
     def get_gpx_points(self):
         points = []
-        gpx_file_name = self.get_gpx_file_name()
+        gpx_file_name = self._get_gpx_file_name_without_path()
         gpx_file = self.filesystem.open(gpx_file_name, 'r')
         gpx = gpxpy.parse(gpx_file)
-        gpx.simplify(20)
+        gpx.simplify(5)
         first_segment = gpx.tracks[0].segments[0]
         for point in first_segment.points:
             new_point = (point.latitude, point.longitude)
@@ -35,8 +44,12 @@ class TrailDirectoryService:
 
     def get_responsive_images(self):
         responsive_images_chunked = []
+        responsive_images_with_path = []
         responsive_images = self.filesystem.listdir('./responsiveImages')[1]
-        responsive_images.sort()
-        for chunk in self.chunk(responsive_images, 3):
+        for responsive_image in responsive_images:
+            image_with_path = path.join(path.basename(self.filesystem.base_location), 'responsiveImages', responsive_image)
+            responsive_images_with_path.append(image_with_path)
+        responsive_images_with_path.sort()
+        for chunk in self.chunk(responsive_images_with_path, 3):
             responsive_images_chunked.append(chunk)
         return responsive_images_chunked
