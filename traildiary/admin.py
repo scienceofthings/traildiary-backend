beb7213aff156d7f88abe4839c14e334583475f8
Gregor Hyneck <supergreg@gmx.de>
2021-04-24 10:45:28 +0200

    Hide admin fields

--- traildiary/admin.py
+++ traildiary/admin.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from .models import Region
 from .models import Trail
 
+
 class TrailAdmin(admin.ModelAdmin):
-    exclude = ('startPositionLat','startPositionLng')
+    exclude = ('gpx_file_name', 'gpx_points', 'start_position', 'images')
+
 
 admin.site.register(Region)
 admin.site.register(Trail, TrailAdmin)
